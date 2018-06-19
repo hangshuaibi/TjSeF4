ab4da337d2853ca5580bb2d3b37ac0212ba5d222
xuhaoyu1 <39218689+xuhaoyu1@users.noreply.github.com>
2018-06-19 20:34:33 +0800

    增加了背景音乐开关

--- AudioControlScene.cpp
+++ AudioControlScene.cpp
@@ -15,7 +15,6 @@ Scene* AudioControl::createScene()
 }
 
 
-
 char* EncodeToUTF81(const char* mbcsStr)
 {
 	wchar_t*  wideStr;
@@ -32,6 +31,7 @@ char* EncodeToUTF81(const char* mbcsStr)
 }
 
 
+
 bool::AudioControl::init()
 {
 
@@ -46,140 +46,18 @@ bool::AudioControl::init()
 
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 
-	//背景音乐标签和滑动条设计
 	//创建“背景音乐”文本并添加为当前层的子节点
 
-	auto music_text = Text::create(EncodeToUTF81("背景音乐"), "Arial", 32);
-	music_text->setPosition(Vec2(visibleSize.width*0.25, visibleSize.height*0.7));
+	auto music_text = Text::create(EncodeToUTF81("背景音乐"), "Arial", 14);
+	music_text->setPosition(Vec2(visibleSize.width*0.5, visibleSize.height*0.7));
 	this->addChild(music_text);
 
-	//创建一个滑动条
-
-	auto music_slider = Slider::create();
-
-	//设置滑动条的纹理
-
-	music_slider->loadBarTexture("sliderTrack.png");
-
-	//设置滑动条的滚轮纹理
-
-	music_slider->loadSlidBallTextures("sliderThumb.png", "sliderThumb.png", "");
-
-	//设置处理滑动条的进度条纹理
-
-	music_slider->loadProgressBarTexture("sliderProgress.png");
-	music_slider->setScale(0.7);
-
-	//处理之前设置的背景音乐音量
-
-	float musicPercent = UserDefault::getInstance()->getFloatForKey("musicPercent");
-
-	//如果是第一次进入设置场景，设置背景音乐滑动条默认初始值为100
-
-	if (musicPercent == 0.0f)
-	{
-		musicPercent = 100.0f;
-	}
-
-	//设置背景音乐滑动条的初始值
-
-	music_slider->setPercent(musicPercent);
-	music_slider->setPosition(Vec2(visibleSize.width*0.6, visibleSize.height*0.7));
-
-	//添加事件监听器，调整背景音乐音量
-
-	music_slider->addEventListener([=](Ref* pSender, Slider::EventType type)
-	{
-
-		//当滑块的百分比发生变化时
-
-		if (type == Slider::EventType::ON_PERCENTAGE_CHANGED)
-		{
-
-			//获得滑动条百分比
-
-			int percent = music_slider->getPercent();
-
-			//设置背景音乐值为滑动条百分比/100，因为Slider的百分比是1~100,而MusicVolume的取值是0.0~1.0
-
-			SimpleAudioEngine::getInstance()->setBackgroundMusicVolume(float(percent) / 100);
-
-			//存储设置的背景音乐值
-
-			UserDefault::getInstance()->setFloatForKey("musicPercent", percent);
-		}
-	});
-	this->addChild(music_slider);
-
-	//音效音量标签
-	//创建“音效音量”文本并添加为当前层的子节点
-	auto sound_text = Text::create(EncodeToUTF81("音效音量"), "Arial", 32);
-	sound_text->setPosition(Vec2(visibleSize.width*0.25, visibleSize.height*0.5));
-	this->addChild(sound_text);
-
-	//创建一个滑动条
-
-	auto effects_slider = Slider::create();
-
-	//设置滑动条的纹理
-
-	effects_slider->loadBarTexture("sliderTrack.png");
-
-	//设置滑动条的滚轮纹理
-
-	effects_slider->loadSlidBallTextures("sliderThumb.png", "sliderThumb.png", "");
-
-	//设置处理滑动条的进度条纹理
-
-	effects_slider->loadProgressBarTexture("sliderProgress.png");
-
-	//获取之前设置的音效音量
-
-	float effectPercent = UserDefault::getInstance()->getFloatForKey("effectPercent");
-
-	//如果是第一次进入设置场景，设置音效滑动条默认初始值为100
-
-	if (effectPercent == 0.0f)
-	{
-		effectPercent = 100.0f;
-	}
-
-	//设置音效滑动条的初始值
-
-	effects_slider->setPercent(effectPercent);
-	effects_slider->setPosition(Vec2(visibleSize.width*0.6, visibleSize.height*0.5));
-	effects_slider->setScale(0.7);
-
-	//添加时间监听器，调整音效音量
-
-	effects_slider->addEventListener([=](Ref* pSender, Slider::EventType type)
-	{
-
-		//当滑块的百分比发生变化时
-
-		if (type == Slider::EventType::ON_PERCENTAGE_CHANGED)
-		{
-
-			//获得滑动条百分比
-
-			int percent = effects_slider->getPercent();
-
-			//设置背景音效值
-
-			SimpleAudioEngine::getInstance()->setEffectsVolume(float(percent) / 100);
-
-			//存储设置的背景音乐值
-
-			UserDefault::getInstance()->setFloatForKey("effectPercent", percent);
-		}
-	});
-	this->addChild(effects_slider);
+	
 
 	//创建“返回"按钮，点击时调用returnMenu函数
 
 	auto return_button = Button::create("b1.jpg");
-	return_button->setPosition(Vec2(visibleSize.width - return_button->getContentSize().width / 2*0.1, return_button->getContentSize().height / 2*0.1));
-	return_button->setScale(0.1);
+	return_button->setPosition(Vec2(visibleSize.width - return_button->getContentSize().width / 2, return_button->getContentSize().height / 2));
 	return_button->addTouchEventListener([](Ref* pSender, Widget::TouchEventType type)
 	{
 		if (type == Widget::TouchEventType::ENDED)
@@ -192,6 +70,30 @@ bool::AudioControl::init()
 		}
 	});
 	this->addChild(return_button);
+
+	// 暂停背景音乐
+	auto pauseMusic = Button::create("musicPause.png");
+	pauseMusic->addTouchEventListener([](Ref* pSender, Widget::TouchEventType type)
+	{
+		if (type == Widget::TouchEventType::ENDED)
+		{
+			SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
+		}
+	});
+	pauseMusic->setPosition(Vec2(visibleSize.width*0.45, visibleSize.height*0.65));
+	this->addChild(pauseMusic);
+
+	// 继续背景音乐
+	auto resumeMusic = Button::create("musicContinue.png");
+	resumeMusic->addTouchEventListener([](Ref* pSender, Widget::TouchEventType type)
+	{
+		if (type == Widget::TouchEventType::ENDED)
+		{
+			SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
+		}
+	});
+	resumeMusic->setPosition(Vec2(visibleSize.width*0.55, visibleSize.height*0.65));
+	this->addChild(resumeMusic);
 
 	//组名
 
